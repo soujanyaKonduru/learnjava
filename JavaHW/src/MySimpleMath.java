3d5a2345705064944c9cf28f0a10dc8c4aabb0ad
soujanyaKonduru <67354949+soujanyaKonduru@users.noreply.github.com>
2020-08-07 17:13:07 -0700

    mysimplemath

---
+++ JavaHW/src/MySimpleMath.java
@@ -0,0 +1,16 @@
+
+public class MySimpleMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x =20;
+		int y =30;
+		int z = x+y;
+		System.out.print("sum of two number: "+ z);
+		int m = x*y;
+		System.out.print("\n Multiplication of two numbers: " + m);
+		
+	}
+
+}
