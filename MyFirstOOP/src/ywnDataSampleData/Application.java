be0b93c9bb38fa52a6cedc50f40adad035e9431c
soujanyaKonduru <67354949+soujanyaKonduru@users.noreply.github.com>
2020-08-09 10:10:57 -0700

    My First OOP project

---
+++ MyFirstOOP/src/ywnDataSampleData/Application.java
@@ -0,0 +1,23 @@
+package ywnDataSampleData;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("learning my own data type");
+		
+		Person person1 = new Person();
+		person1.name = "sai1";
+		person1.age = 12;
+		    
+		Person person2 = new Person();
+		person2.name = "sai2";
+		person2.age = 15;
+
+		    System.out.println(person1.name + " age is:" + person1.age);
+		    System.out.println(person2.name + " age is:" + person2.age);
+		
+	
+	}
+
+}
---
+++ MyFirstOOP/src/ywnDataSampleData/Person.java
@@ -0,0 +1,11 @@
+package ywnDataSampleData;
+
+public class Person {
+
+	String name;
+    int age;
+    int grade;
+    int height;
+    int weight;
+	
+}
