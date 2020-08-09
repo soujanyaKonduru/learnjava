8f45f126eebd0eaf3ecfe2337248639be7b43d53
soujanyaKonduru <67354949+soujanyaKonduru@users.noreply.github.com>
2020-08-09 14:29:23 -0700

    PrintDate

---
+++ JDKPackage/src/JDKDateClass.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+
+
+public class JDKDateClass {
+	public static void main(String[] args)
+	{
+		
+		
+	    java.util.Date date;
+	    date = new java.util.Date();
+	    System.out.println("today date is" + date.toString());
+    }
+
+}
---
+++ JDKPackage/src/superMath/Math.java
@@ -0,0 +1,10 @@
+package superMath;
+
+public class Math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
